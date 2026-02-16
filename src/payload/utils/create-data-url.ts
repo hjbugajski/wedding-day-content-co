5c6778cbbe06f481bd507fb165febbff064564f7
Henry Bugajski <henry.bug.dev@gmail.com>
2026-02-16 17:07:41 -0500

    fix: image size generation

--- src/payload/utils/create-data-url.ts
+++ src/payload/utils/create-data-url.ts
@@ -1,10 +1,9 @@
 import sharp from 'sharp';
 
-export async function createDataUrl(url: string, mimeType?: string | null): Promise<string> {
+export async function createDataUrl(url: string, mimeType?: string | null): Promise<string | null> {
   const image = await fetch(url);
   const imageBuffer = await image.arrayBuffer();
   const sharpBuffer = await sharp(imageBuffer).resize(50).toBuffer();
-  const dataUrl = `data:${mimeType || 'image/png'};base64,${sharpBuffer.toString('base64')}`;
 
-  return dataUrl;
+  return `data:${mimeType || 'image/png'};base64,${sharpBuffer.toString('base64')}`;
 }
