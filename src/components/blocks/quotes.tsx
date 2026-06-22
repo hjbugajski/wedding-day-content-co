947e55d4060e2d674c8322dafef38b7549be5071
Henry Bugajski <henry.bug.dev@gmail.com>
2026-06-21 20:07:26 -0400

    fix: revert back to masonry (#117)

--- src/components/blocks/quotes.tsx
+++ src/components/blocks/quotes.tsx
@@ -1,12 +1,5 @@
 import type { RichTextComponent } from '@/components/rich-text/types';
 import { Blockquote, BlockquoteBody, BlockquoteFooter } from '@/components/ui/blockquote';
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from '@/components/ui/carousel';
 import type { PayloadQuotesBlock } from '@/payload/payload-types';
 
 interface QuotesBlockProps extends PayloadQuotesBlock {
@@ -19,23 +12,15 @@ export function QuotesBlock({ quotes, RichText }: QuotesBlockProps) {
   }
 
   return (
-    <Carousel aria-label="Testimonials">
-      <CarouselContent className="items-stretch py-6">
-        {quotes.map(({ client, content, id }) => (
-          <CarouselItem key={id} className="basis-11/12 sm:basis-1/2 lg:basis-1/3">
-            <Blockquote className="h-full">
-              <BlockquoteBody>
-                <RichText data={content} />
-              </BlockquoteBody>
-              <BlockquoteFooter>{client}</BlockquoteFooter>
-            </Blockquote>
-          </CarouselItem>
-        ))}
-      </CarouselContent>
-      <div className="flex justify-between py-4 md:py-0">
-        <CarouselPrevious />
-        <CarouselNext />
-      </div>
-    </Carousel>
+    <div className="masonry">
+      {quotes.map(({ client, content, id }) => (
+        <Blockquote key={id} className="masonry-item">
+          <BlockquoteBody>
+            <RichText data={content} />
+          </BlockquoteBody>
+          <BlockquoteFooter>{client}</BlockquoteFooter>
+        </Blockquote>
+      ))}
+    </div>
   );
 }
